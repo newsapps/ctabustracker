4fd715a32bec7df0615377102eb1cd03f6d17708
cgroskopf <cgroskopf@tribune.com>
2010-01-11 16:57:36 -0600

    Tweaked setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,10 @@ from setuptools import setup, find_packages
 
 setup(name='ctabustracker',
     version='0.1dev',
-    description='A thin wrapper around the CTA Bus Tracker API.',
+    description='A python wrapper for the Chicago Transit Authority\'s Bustracker API.',
     long_description='',
     author='Christopher Groskopf',
     author_email='cgroskopf@tribune.com',
-    url='???',
     packages=find_packages(),
     install_requires=['BeautifulSoup'],
     )
